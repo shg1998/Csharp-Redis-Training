0b771e23e0763988754315a6a4dc9f1ead70592f
Mohamad Nezhadhendi <m.nezhad@saadat.local>
2022-01-09 17:25:13 +0330

    simple Console App Added !

---
+++ Redis_Csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+
+namespace Redis_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var redis = new RedisClient())
+            {
+                var redisUsers = redis.As<Person>();
+                var mohammad = new Person {Id = redisUsers.GetNextSequence(), Name = "Mohammad"};
+                var ali = new Person {Id = redisUsers.GetNextSequence(), Name = "Ali"};
+                var hassan = new Person {Id = redisUsers.GetNextSequence(), Name = "Hassan" };
+
+                redisUsers.Store(mohammad);
+                redisUsers.Store(ali);
+                redisUsers.Store(hassan);
+
+                var allThePeople = redisUsers.GetAll();
+                Console.WriteLine(allThePeople.Dump());
+            }
+        }
+    }
+
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
